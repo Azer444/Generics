b2c51ede5ffac8a915e5c0529e2816e50af1773b
Azer444 <Azerrh@code.edu.az>
2022-07-08 00:30:03 +0400

    Generic

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region MyRegion
+
+
+            //        StringList list = new StringList();
+            //        list.Add("Azer");
+            //        list.Add("Humbetov");
+            //        list.GetAll();
+            //    }
+            //}
+
+
+            //public class StringList
+            //{
+            //    private string[] arr;
+
+
+            //    public StringList()
+            //    {
+            //        arr = new string[0];
+            //    }
+            //    public void Add(string str)
+            //    {
+            //        Array.Resize(ref arr, arr.Length + 1);
+            //        arr[arr.Length - 1] = str;
+            //    }
+            //    public void GetAll()
+            //    {
+            //        foreach (var item in arr)
+            //        {
+            //            Console.WriteLine(item);
+            //        }
+            #endregion
+
+            #region Lab
+
+
+            //        ListData<Car> listData = new ListData<Car>();
+            //        listData.AddData(new Car { model = "BMW" });
+            //        listData.GetData();
+
+
+
+            //    }
+
+            //}
+            //public class ListData<T> where T : Base
+            //{
+            //    private T[] arr;
+            //    public ListData()
+            //    {
+            //        arr = new T[0];
+            //    }
+            //    public void AddData(T data)
+            //    {
+            //        Array.Resize(ref arr, arr.Length + 1);
+            //        arr[arr.Length - 1] = data;
+            //    }
+            //    public void GetData()
+            //    {
+            //        foreach (var item in arr)
+            //        {
+            //            Console.WriteLine(item.model);
+            //        }
+            //    }
+
+            //}
+            //public class Base
+            //{
+            //    public string model;
+            //}
+            //public class Car : Base
+            //{
+
+            //}
+            #endregion
+        }
+    }
+}
